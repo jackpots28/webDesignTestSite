e51be92ed4f07df094d151e993bb0ff99539628b
Jack Sims <24850877+jackpots28@users.noreply.github.com>
2018-11-06 14:33:41 -0600

    Add files via upload

---
+++ root/java-script/jscript_nav.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "230px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
